b5e5b5de979ed74034013241a6a73fec65825c70
Gurpreet Singh <gur.singh2022@gmail.com>
2019-12-07 13:48:39 +0000

    Added addition and substration function.

---
+++ Maths.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Maths 
+{ 
+    // Your program begins with a call to main(). 
+    public static void main(String args[]) 
+    {
+        // // Prints "Hello, World" to the terminal window.
+        // System.out.println("Hello, World");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Addition? ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + addition(num1, num2));
+
+        System.out.println("Substraction? ");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        System.out.println(num1 + " - " + num2 + " = " + substraction(num1, num2));
+
+        sc.close();
+    }
+
+    public static int addition(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+    public static int substraction(int num1, int num2) {
+        return num1 - num2;
+    }
+} 
